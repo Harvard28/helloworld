b83b0d615c3b63d72f518873a838db7ae2ebaefa
Harvard28 <144681667+Harvard28@users.noreply.github.com>
2023-09-20 18:57:02 +0700

    Vu Tuan Anh

---
+++ Demo/lab2009/lab1/d2odd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+ int main(){
+    int num ,res;
+    printf("Nhap vao mot so di ban oi..");
+    scanf("%d",&num);
+    res = num % 2;
+    if (res == 0)
+         printf("so chan nha ban \n");
+    else
+        printf("so le luon nay \n");
+    return 1;
+}
